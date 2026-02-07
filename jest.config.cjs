e501fd539bd5848980d7ca94c55fb9e1aeb5776e
Daniel Evans <me@daredoes.work>
2026-02-06 23:25:44 -0800

    Add jest configuration and ignore cache directory
    When merged this commit will:
    - Add jest configuration with custom cache directory
    - Add .cache/ to .gitignore
    - Fix jest caching issues on macOS

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ["<rootDir>/jestSetup.ts"]
+  setupFilesAfterEnv: ["<rootDir>/jestSetup.ts"],
+  cacheDirectory: './.cache',
 };
